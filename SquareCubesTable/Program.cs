248949a52d81c70b4194f974b9420d56c3a04f9a
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-03-31 09:40:58 +0500

    solution done

--- SquareCubesTable/Program.cs
+++ SquareCubesTable/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class C3Q29
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Number\tSquare\tCube");
+        Console.WriteLine(0 + "\t" + (0 * 0) + "\t" + (0 * 0 * 0));
+        Console.WriteLine(1 + "\t" + (1 * 1) + "\t" + (1 * 1 * 1));
+        Console.WriteLine(2 + "\t" + (2 * 2) + "\t" + (2 * 2 * 2));
+        Console.WriteLine(3 + "\t" + (3 * 3) + "\t" + (3 * 3 * 3));
+        Console.WriteLine(4 + "\t" + (4 * 4) + "\t" + (4 * 4 * 4));
+        Console.WriteLine(5 + "\t" + (5 * 5) + "\t" + (5 * 5 * 5));
+        Console.WriteLine(6 + "\t" + (6 * 6) + "\t" + (6 * 6 * 6));
+        Console.WriteLine(7 + "\t" + (7 * 7) + "\t" + (7 * 7 * 7));
+        Console.WriteLine(8 + "\t" + (8 * 8) + "\t" + (8 * 8 * 8));
+        Console.WriteLine(9 + "\t" + (9 * 9) + "\t" + (9 * 9 * 9));
+        Console.WriteLine(10 + "\t" + (10 * 10) + "\t" + (10 * 10 * 10));
+    }
+}
